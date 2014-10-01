371734eb18c4f309c32ff2f198cd1fd18dafd007
nancy.taylor <ntaylor120@gmail.com>
2014-10-01 06:08:22 -0500

    continuing to build the password generating logic, fixed problem in array builder, starting to add check logic.  /nt

--- logic.php
+++ logic.php
@@ -1,7 +1,17 @@
 <?php
 
+//  *****************  Determines how many words in the password, and whether there will be symbols, numbers or uppercase
+
+
+
+//$$$$$$$$$$  there is something wrong with the code here:
+
 if (isset($_POST['count'])){
 	$count=$_POST ['count'];
+} else {
+	$count = 1;
+	# takes the number from the number of words field, and send that to the 'array generator' (see below)
+	#if no number is supplied, assume one word
 }
 
 if (isset($_POST['uppercase'])){
@@ -25,6 +35,36 @@ if (isset($_POST['number'])){
 	$number = false;
 }
 
+//check - the count from the form should be a number, not a letter, and it shouldn't be larger than ...
+
+
+
+
+if ($words = file('words.txt')){
+						//use file() to take the words.txt file and turn every line into an element in the array $words:
+
+	
+	$selected_words = [];  //create an empty array that will hold the list of words for the password
+
+
+	for ($i=0; $i < $count ; $i++) {    //get number of words for the $selected_words array, based on $count
+
+		// generate random number from zero to dictionary size:
+
+		$max = count($words) -1; //determine the size of the array
+		$rand = rand(0, $max);  //get a random number between zero and the largest element in the array
+
+		//add item to an array:
+
+		$word = $words[$rand];   //find a random word
+
+		echo $word . '<br />';  //test to make sure that the logic is working
+
+		array_push($selected_words, $word); //add this to the $selected_words array
+
+		}
+
+	}
 
 
 
@@ -40,3 +80,5 @@ if (isset($_POST['number'])){
 // update variables to send back
 
 // cleanup with any helper functions
+
+//you have 4 days to complete this project!  (no pressure)
